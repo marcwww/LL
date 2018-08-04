78070c16840345401a93f407995b143cf171e1b2
Marc Wang <474733787@qq.com>
2018-08-04 16:29:33 +0800

    max pooling net

--- opts.py
+++ opts.py
@@ -23,7 +23,7 @@ def train_opts(parser):
     group.add_argument('-min_freq', type=int, default=5)
     group.add_argument('-nepoch', type=int, default=10)
     group.add_argument('-save_per', type=int, default=5)
-    group.add_argument('-name', type=str, default='bigru')
+    group.add_argument('-name', type=str, default='pooling')
     group.add_argument('-gpu', type=int, default=-1)
     group.add_argument('-lr', type=float, default=1e-3)
     # group.add_argument('-lm_coef', type=float, default=0.5)
