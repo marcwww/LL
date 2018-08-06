8bddf835c74c829ae117385b8b3846d20d098528
Marc Wang <474733787@qq.com>
2018-08-06 23:33:52 +0800

    mnist

---
+++ training_cv.py
@@ -0,0 +1,141 @@
+import torch
+from sklearn.metrics import f1_score, \
+    precision_score, \
+    recall_score, \
+    accuracy_score
+from torch.nn.utils import clip_grad_norm_
+import utils
+import torchtext
+import os
+import sys
+from macros import *
+from torch import nn
+import numpy as np
+
+def valid_mnist(model, valid_loader, task_permutation):
+    model.eval()
+    pred_lst = []
+    true_lst = []
+
+    with torch.no_grad():
+        for i, (input, lbl) in enumerate(valid_loader):
+            input = input.view(-1, MNIST_DIM)
+            input = input[:, task_permutation]
+            lbl = lbl.squeeze(0)
+            # probs: (bsz, 3)
+
+            out = model(input)
+
+            pred = out.max(dim=1)[1].cpu().numpy()
+            lbl = lbl.cpu().numpy()
+            pred_lst.extend(pred)
+            true_lst.extend(lbl)
+
+    accurracy = accuracy_score(true_lst, pred_lst)
+    precision = precision_score(true_lst, pred_lst, average='macro')
+    recall = recall_score(true_lst, pred_lst, average='macro')
+    f1 = f1_score(true_lst, pred_lst, average='macro')
+
+    return accurracy, precision, recall, f1
+
+def train_domain_mnist(model, dataloaders, opt, domain,
+                       task_permutations, criterion, optim):
+
+    train_loader = dataloaders['train']
+    valid_loader = dataloaders['valid']
+
+    flog = opt.name + '.log'
+    with open(os.path.join(RES, flog), 'a+') as print_to:
+
+        print('\r')
+        print('{\'domain\':%d}' % domain)
+        print('{\'domain\':%d}' % domain, file=print_to)
+        # print('--' * 10 + ('domain %d' % domain) + '--' * 10)
+        # print('--' * 10 + ('domain %d' % domain) + '--' * 10, file=print_to)
+
+        best_model = ''
+        best_performance = 0
+        best_metrics = {}
+        best_epoch = 0
+        for epoch in range(opt.nepoch):
+            for i, (input, lbl) in enumerate(train_loader):
+                input = input.view(-1, MNIST_DIM)
+                input = input[:, task_permutations[domain]]
+                model.train()
+
+                model.zero_grad()
+                out = model(input)
+                loss = criterion(out, lbl.squeeze(0))
+                loss.backward()
+
+                # clip_grad_norm_(model.parameters(), 5)
+                optim.step()
+
+                utils.progress_bar(i / len(train_loader), loss.item(), epoch)
+
+                if (i + 1) % int(1 / 4 * len(train_loader)) == 0:
+                    # valid
+                    accurracy, precision, recall, f1 = \
+                        valid_mnist(model, valid_loader, task_permutations[domain])
+                    performance = {'accuracy':accurracy,
+                                   'precision':precision,
+                                   'recall':recall,
+                                   'f1':f1}
+
+                    if performance[opt.metric] > best_performance:
+                        print('\r')
+                        print(
+                            '{\'Epoch\':%d, \'Domain\':%d, \'Format\':\'a/p/r/f\', \'Metrics\':[%4f, %4f, %4f, %4f]}' %
+                            (epoch, domain, accurracy, precision, recall, f1))
+
+                        # save model
+                        basename = "up-to-domain-{}-epoch-{}".format(domain, epoch)
+                        model_fname = basename + ".model"
+                        torch.save(model.state_dict(), model_fname)
+
+                        best_performance = performance[opt.metric]
+                        best_model = model_fname
+                        best_metrics[domain] = (accurracy, precision, recall, f1)
+                        best_epoch = epoch
+
+                        # valid the rest
+                        for d in range(opt.ndomains):
+                            if d == domain:
+                                continue
+
+                            accurracy, precision, recall, f1 =\
+                                valid_mnist(model, valid_loader, task_permutations[d])
+
+                            print('{\'Epoch\':%d, \'Domain\':%d, \'Format\':\'a/p/r/f\', \'Metrics\':[%4f, %4f, %4f, %4f]}' %
+                                  (epoch, d, accurracy, precision, recall, f1))
+
+                            best_metrics[d] = (accurracy, precision, recall, f1)
+
+                    print_to.flush()
+
+        # logging the best performance on the current domain
+        for d in range(domain+1):
+            accurracy, precision, recall, f1 = best_metrics[d]
+            print('{\'Epoch\':%d, \'Domain\':%d, \'Format\':\'a/p/r/f\', \'Metrics\':[%4f, %4f, %4f, %4f]}' %
+                  (best_epoch, d, accurracy, precision, recall, f1), file=print_to)
+
+    location = {'cuda:' + str(opt.gpu): 'cuda:' + str(opt.gpu)} if opt.gpu != -1 else 'cpu'
+    model_dict = torch.load(best_model, map_location=location)
+    model.load_state_dict(model_dict)
+
+def train_ll_mnist(model, dataloaders, opt, optim):
+
+    domains = range(opt.ndomains)
+
+    location = opt.gpu if torch.cuda.is_available() and opt.gpu != -1 else 'cpu'
+    device = torch.device(location)
+
+    task_permutations = []
+    for _ in domains:
+        task_permutations.append(np.random.permutation(784))
+
+    for domain in domains:
+        criterion = nn.CrossEntropyLoss()
+        train_domain_mnist(model, dataloaders,
+                     opt, domain, task_permutations, criterion, optim)
+
---
+++ training_text.py
@@ -0,0 +1,252 @@
+import torch
+from sklearn.metrics import f1_score, \
+    precision_score, \
+    recall_score, \
+    accuracy_score
+from torch.nn.utils import clip_grad_norm_
+import utils
+import torchtext
+import os
+import sys
+from macros import *
+from torch import nn
+
+def valid(model, valid_iter):
+    model.eval()
+    pred_lst = []
+    true_lst = []
+
+    with torch.no_grad():
+        for i, sample in enumerate(valid_iter):
+            txt, lbl = sample.txt, sample.lbl
+            lbl = lbl.squeeze(0)
+            # probs: (bsz, 3)
+
+            if model.with_lm:
+                probs, _ = model(txt[:-1])
+            else:
+                probs = model(txt)
+
+            pred = probs.max(dim=1)[1].cpu().numpy()
+            lbl = lbl.cpu().numpy()
+            pred_lst.extend(pred)
+            true_lst.extend(lbl)
+
+    accurracy = accuracy_score(true_lst, pred_lst)
+    precision = precision_score(true_lst, pred_lst, average='macro')
+    recall = recall_score(true_lst, pred_lst, average='macro')
+    f1 = f1_score(true_lst, pred_lst, average='macro')
+
+    return accurracy, precision, recall, f1
+
+def train(model, iters, opt, domain, criterions, optim):
+    train_iter = iters['train']
+    valid_iter = iters['valid']
+    criterion = criterions['senti']
+    criterion_lm = criterions['lm']
+
+    for epoch in range(opt.nepoch):
+        for i, sample in enumerate(train_iter):
+            model.train()
+            txt, lbl = sample.txt, sample.lbl
+
+            model.zero_grad()
+            # probs: (bsz, 3)
+
+            logits=None
+            if model.with_lm:
+                probs, logits = model(txt[:-1])
+            else:
+                probs = model(txt)
+
+            loss = criterion(probs, lbl.squeeze(0))
+
+            if model.with_lm:
+                voc_size = model.embedding.num_embeddings
+                tar = txt[1:]
+                loss_lm = criterion_lm(logits.view(-1, voc_size), tar.view(-1))
+                (loss+loss_lm).backward()
+            else:
+                loss.backward()
+
+            clip_grad_norm_(model.parameters(), 5)
+            optim.step()
+
+            percent = i/len(train_iter)
+
+            utils.progress_bar(percent, loss.item(), epoch)
+
+            if (i+1) % int(1/4 * len(train_iter)) == 0 :
+                # print('\r')
+                accurracy, precision, recall, f1 = \
+                    valid(model, valid_iter)
+                print('{\'Epoch\':%d, \'Domain\':%d, \'Format\':\'a/p/r/f\', \'Metrics\':[%.4f, %.4f, %.4f, %.4f]}' %
+                      (epoch, domain, accurracy, precision, recall, f1))
+
+
+        if (epoch + 1) % opt.save_per == 0:
+            basename = "up-to-domain-{}-epoch-{}".format(domain, epoch)
+            model_fname = basename + ".model"
+            torch.save(model.state_dict(), model_fname)
+
+def train_domain(model, iters, opt, domain, criterion, optim):
+
+    flog = opt.name + '.log'
+    with open(os.path.join(RES, flog), 'a+') as print_to:
+
+        train_iter = iters['train']
+        valid_iters = iters['valids'][:domain+1]
+        # valid_iters = iters['valids']
+
+        TXT = train_iter.dataset.fields['txt']
+        criterion_lm = nn.CrossEntropyLoss(ignore_index=TXT.vocab.stoi[PAD])
+
+        print('\r')
+        print('{\'domain\':%d}' % domain)
+        print('{\'domain\':%d}' % domain, file=print_to)
+        # print('--' * 10 + ('domain %d' % domain) + '--' * 10)
+        # print('--' * 10 + ('domain %d' % domain) + '--' * 10, file=print_to)
+
+        best_model = ''
+        best_performance = 0
+        best_metrics = {}
+        best_epoch = 0
+        for epoch in range(opt.nepoch):
+            for i, sample in enumerate(train_iter):
+                model.train()
+                txt, lbl = sample.txt, sample.lbl
+
+                model.zero_grad()
+                # probs: (bsz, 3)
+                logits = None
+                if model.with_lm:
+                    probs, logits = model(txt[:-1])
+                else:
+                    probs = model(txt)
+
+                loss = criterion(probs, lbl.squeeze(0))
+
+                if model.with_lm:
+                    voc_size = model.embedding.num_embeddings
+                    tar = txt[1:]
+                    loss_lm = criterion_lm(logits.view(-1, voc_size), tar.view(-1))
+                    (loss + loss_lm).backward()
+                else:
+                    loss.backward()
+                clip_grad_norm_(model.parameters(), 5)
+                optim.step()
+
+                utils.progress_bar(i / len(train_iter), loss.item(), epoch)
+
+                if (i + 1) % int(1 / 4 * len(train_iter)) == 0:
+                    # valid
+                    accurracy, precision, recall, f1 = \
+                        valid(model, valid_iters[domain])
+                    performance = {'accuracy':accurracy,
+                                   'precision':precision,
+                                   'recall':recall,
+                                   'f1':f1}
+
+                    if performance[opt.metric] > best_performance:
+                        print('\r')
+                        print(
+                            '{\'Epoch\':%d, \'Domain\':%d, \'Format\':\'a/p/r/f\', \'Metrics\':[%4f, %4f, %4f, %4f]}' %
+                            (epoch, domain, accurracy, precision, recall, f1))
+
+                        # save model
+                        basename = "up-to-domain-{}-epoch-{}".format(domain, epoch)
+                        model_fname = basename + ".model"
+                        torch.save(model.state_dict(), model_fname)
+
+                        best_performance = performance[opt.metric]
+                        best_model = model_fname
+                        best_metrics[domain] = (accurracy, precision, recall, f1)
+                        best_epoch = epoch
+
+                        # valid the rest
+                        for d, valid_iter in enumerate(valid_iters):
+                            if d == domain:
+                                continue
+
+                            accurracy, precision, recall, f1 =\
+                                valid(model, valid_iter)
+
+                            print('{\'Epoch\':%d, \'Domain\':%d, \'Format\':\'a/p/r/f\', \'Metrics\':[%4f, %4f, %4f, %4f]}' %
+                                  (epoch, d, accurracy, precision, recall, f1))
+
+                            best_metrics[d] = (accurracy, precision, recall, f1)
+
+                    print_to.flush()
+
+        # logging the best performance on the current domain
+        for d in range(domain+1):
+            accurracy, precision, recall, f1 = best_metrics[d]
+            print('{\'Epoch\':%d, \'Domain\':%d, \'Format\':\'a/p/r/f\', \'Metrics\':[%4f, %4f, %4f, %4f]}' %
+                  (best_epoch, d, accurracy, precision, recall, f1), file=print_to)
+
+    location = {'cuda:' + str(opt.gpu): 'cuda:' + str(opt.gpu)} if opt.gpu != -1 else 'cpu'
+    model_dict = torch.load(best_model, map_location=location)
+    model.load_state_dict(model_dict)
+
+def train_ll(model, uiters, info, opt, optim):
+
+    flog = opt.name + '.log'
+    with open(os.path.join(RES, flog), 'w') as print_to:
+        pass
+
+    utrain_iter = uiters['train']
+    uvalid_iter = uiters['valid']
+    train_ranges = info['train_ranges']
+    valid_ranges = info['valid_ranges']
+    fields = utrain_iter.dataset.fields
+    bsz = utrain_iter.batch_size
+    device = utrain_iter.device
+
+    domains = range(len((train_ranges)))
+    # domains = [opt.test_domain]
+    # for d in range(len(train_ranges)):
+    #     if d != opt.test_domain:
+    #         domains.append(d)
+
+    examples_utrain = utrain_iter.dataset.examples
+    examples_uvalid = uvalid_iter.dataset.examples
+    train_iters = []
+    valid_iters = []
+    for domain in domains:
+        begin_train, end_train = train_ranges[domain]
+        begin_valid, end_valid = valid_ranges[domain]
+
+        examples_train = examples_utrain[begin_train: end_train]
+        examples_valid = examples_uvalid[begin_valid: end_valid]
+
+        train = torchtext.data.Dataset(examples_train, fields)
+        valid = torchtext.data.Dataset(examples_valid, fields)
+
+        train_iter = torchtext.data.Iterator(train, batch_size=bsz,
+                                sort=False, repeat=False,
+                                sort_key=lambda x: len(x.txt),
+                                sort_within_batch=True,
+                                shuffle=False,
+                                device=device)
+        valid_iter = torchtext.data.Iterator(valid, batch_size=bsz,
+                                             sort=False, repeat=False,
+                                             sort_key=lambda x: len(x.txt),
+                                             sort_within_batch=True,
+                                             shuffle=False,
+                                             device=device)
+
+        train_iters.append(train_iter)
+        valid_iters.append(valid_iter)
+
+    for domain in domains:
+
+        location = opt.gpu if torch.cuda.is_available() and opt.gpu != -1 else 'cpu'
+        device = torch.device(location)
+
+        weights = utils.balance_bias(train_iters[domain])
+        print(weights)
+        criterion = nn.CrossEntropyLoss(weight=torch.Tensor(weights).to(device))
+        train_domain(model, {'train': train_iters[domain],
+                             'valids': valid_iters},
+                     opt, domain, criterion, optim)
+
